25bd92e5c23dd4dd2093c1cf7f0e45fc79abe1ab
KevXtroy <KevXtroyG@gmail.com>
2019-06-01 16:50:15 -0700

    Update addai.js

--- addai.js
+++ addai.js
@@ -1,3 +1,3 @@
 network.send("addAi", "text", "alpha", [{}]);
 
-network.send("addAi", 'y e e e e e e e e e e e t'.replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e'), "alpha", [{}]);
+network.send("addAi", 'y e e e e e e e e e e e e e e e e e e e e e e e e t'.replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e'), "beta", [{}]);
