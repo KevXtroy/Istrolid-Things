879803a20a8272d7549f00b134e2babfa413ebd0
KevXtroy <KevXtroyG@gmail.com>
2019-06-01 16:42:58 -0700

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,3 @@
 rootNet.send("message", {text: `${1+1+1+1}`, channel: chat.channel});
+
+rootNet.send("message", {text: 'y e e e e e e e e e e e t'.replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e'), channel: chat.channel});
