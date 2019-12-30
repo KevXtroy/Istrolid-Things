6bfc8d7cca125bba7bf50fd47e5e6f5780ef63c9
KevXtroy <KevXtroyG@gmail.com>
2019-12-29 21:55:47 -0700

    Update bible.js

--- bible.js
+++ bible.js
@@ -1,5 +1,8 @@
 
-fetch("https://dailyverses.net/random-bible-verse")
-.then(response => response.text())
-.then(response => console.log(response.slice(response.indexOf('bibleVerse">') + 12, response.indexOf('<div class="bibleChapter"')).split("<br />").join("\n")))
-.catch(err => console.log(err));
+function getVerse() {
+  var output = "Error :(";
+  fetch("https://dailyverses.net/random-bible-verse")
+   .then(response => response.text())
+   .then(response => rootNet.send("message", {text: response.slice(response.indexOf('bibleVerse">') + 12, response.indexOf('<div class="bibleChapter"')).split("<br />").join("\n") + " -" + response.slice(response.indexOf('<div class="bibleChapter"') + 24, response.slice(response.indexOf('<div class="bibleChapter"') + 24).indexOf('</a>')).split("<br />").join("\n"), channel: chat.channel}))
+   .catch(err => console.log(err));
+}
