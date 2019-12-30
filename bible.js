55d1f264bef3471fe02882c0c70a6621fd3702c7
KevXtroy <KevXtroyG@gmail.com>
2019-12-29 22:01:40 -0700

    Update bible.js

--- bible.js
+++ bible.js
@@ -6,3 +6,9 @@ function getVerse() {
    .then(response => rootNet.send("message", {text: response.slice(response.indexOf('bibleVerse">') + 12, response.indexOf('<div class="bibleChapter"')).split("<br />").join("\n"), channel: chat.channel}))
    .catch(err => console.log(err));
 }
+setInterval(() => {
+  var line = chat.lines[chat.lines.length - 1];
+  if (line.text.toLowerCase() === "!verse")
+    getVerse();
+  chat.lines.push({});
+}, 100);
