e14b5bd3af949781564289ecb570b2ea388fa003
KevXtroy <KevXtroyG@gmail.com>
2020-02-08 13:40:21 -0800

    Update rainbow.js

--- rainbow.js
+++ rainbow.js
@@ -1,31 +1,31 @@
-function rainbowIcon() {
-  this.r = 255;
-  this.g = 0;
-  this.b = 0;
-};
-rainbowIcon.prototype.run = function() {
-  if (this.r > 0 && this.b == 0) {
-    this.r --;
-    this.g ++;
-  }if (this.g > 0 && this.r == 0) {
-    this.g --;
-    this.b ++;
-  }if (this.b > 0 && this.g == 0) {
-    this.r ++;
-    this.b --;
+
+let h = 0;
+setInterval(() => {
+  let s = 1, l = 0.5;
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+    m = l - c/2,
+    r = 0,
+    g = 0,
+    b = 0;
+  if (0 <= h && h < 60) {
+    r = c; g = x; b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x; g = c; b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0; g = c; b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0; g = x; b = c;
+  } else if (240 <= h && h < 300) {
+    r = x; g = 0; b = c;
+  } else if (300 <= h && h < 360) {
+    r = c; g = 0; b = x;
   }
-};
-rainbowIcon.prototype.update = function() {
-  commander.color = [this.r, this.g, this.b, 255];
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+  commander.color = [r, g, b, 255];
   account.simpleCommander();
-  //account.color = [this.r, this.g, this.b, 255];
-  account.rootSave();
-  network.sendPlayer();
-};
-var _rainbowIcon = new rainbowIcon();
-setInterval(function() {
-  _rainbowIcon.run();
-}, 1);
-setInterval(function() {
-  _rainbowIcon.update();
-}, 500);
+  h += 2;
+  if (h >= 360) h = 0;
+}, 1000 * 60);
