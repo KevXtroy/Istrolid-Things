474d5dd9ab419c69f1d96dd7ee2a1fd6a2bcb785
KevXtroy <KevXtroyG@gmail.com>
2020-03-21 22:14:23 -0700

    Update addai.js

--- addai.js
+++ addai.js
@@ -1,3 +1,1 @@
 network.send("addAi", "text", "alpha", [{}]);
-
-network.send("addAi", 'y e e e e e e e e e e e e e e e e e e e e e e e e t'.replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e').replace(/e/g, 'e e e e e e e e e e e'), "beta", [{}]);
